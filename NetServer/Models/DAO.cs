a79b72b46eda2eab6543983a1f2b76765a83d7fe
Aego <raphael.ingardin.ri@gmail.com>
2019-06-24 17:18:34 +0200

    Lecture bdd

--- NetServer/Models/DAO.cs
+++ NetServer/Models/DAO.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,42 +11,35 @@ namespace NetServer.Models
 {
     public class DAO
     {
-        string Conx = "Server=192.168.1.77;Port=5432;Database=calc;User Id=admin;Password=admin;";
+        string Conx = "Server=10.151.129.35;Port=5432;Database=calc;User Id=admin;Password=admin;";
         NpgsqlCommand MyCmd = null;
         NpgsqlConnection MyCnx = null;
 
-        public void InsertPersons(string name)
-        {
-            MyCnx = new NpgsqlConnection(Conx);
-            //La valeur DEFAULT parce que la propriété id est auto incrémenté "INSERT INTO \"Test\"(id,name) values(DEFAULT,:name)"
-            string insert = "INSERT INTO \"Test\"(id,name) values(DEFAULT,:name)";
-            MyCnx.Open();
-            MyCmd = new NpgsqlCommand(insert, MyCnx);
-            MyCmd.Parameters.Add(new NpgsqlParameter("@name", NpgsqlDbType.Varchar)).Value = @name;
-            MyCmd.ExecuteNonQuery(); //Exécution
-            MyCnx.Close();
-        }
+        
 
-        public void SelectAllDevices()
+        public ArrayList RetreiveValues(string device, int size)
         {
+            ArrayList list = new ArrayList();
             MyCnx = new NpgsqlConnection(Conx);
             MyCnx.Open();
-            string select = "SELECT * FROM \"Test\"";
+            string select = "SELECT value, date FROM \"average\" WHERE Id_Device = " + device + "ORDER BY date LIMIT " + size;
             MyCmd = new NpgsqlCommand(select, MyCnx);
             var reader = MyCmd.ExecuteReader();
             if (reader.HasRows)
             {
                 while (reader.Read())
                 {
-                    Console.WriteLine("{0}\t{1}", reader.GetInt32(0), reader.GetString(1));
+                    Measure measure = new Measure();
+                    measure.value = reader.GetInt32(0);
+                    measure.date = reader.GetDateTime(1);
+                    list.Add(measure);
                 }
             }
-            else
-            {
-                Console.WriteLine("No rows found.");
-            }
+            
             reader.Close();
             MyCnx.Close();
+
+            return list;
         }
     }
 }
