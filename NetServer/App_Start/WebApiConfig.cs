f2e9d29c0bf6fba0ffd277334a50924eb306723a
Aego <raphael.ingardin.ri@gmail.com>
2019-06-24 10:58:06 +0200

    testCommentaire

--- NetServer/App_Start/WebApiConfig.cs
+++ NetServer/App_Start/WebApiConfig.cs
@@ -18,6 +18,8 @@ namespace NetServer
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
+
+                //test
             );
         }
     }
