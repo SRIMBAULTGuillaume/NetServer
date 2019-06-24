9364603894caeb6a9c3dde73fc705b4d991a7392
guillaumesibot <guillaumesibot@gmail.com>
2019-06-24 09:42:22 +0200

    second try

--- CalcEngConsole/Program.cs
+++ CalcEngConsole/Program.cs
@@ -13,9 +13,6 @@ namespace CalcEngConsole
 {
     class Program
     {
-
-      
-
         static void Main(string[] args)
         {
             DAO per = new DAO();    //Instaciation
@@ -29,8 +26,27 @@ namespace CalcEngConsole
 
         }
 
-        
+        public static void SreenDevices()
+        {
+            //TODO passer une liste rempli de devices
+            var MyDevices = new List<Metric>();
+            MyDevices.Add(new Metric(42, DateTime.Now, 20));
+            MyDevices.Add(new Metric(42, DateTime.Now, 18));
+            MyDevices.Add(new Metric(47, DateTime.Now, 18));
 
+            var rand = new Random();
+            for (int i = 0; i<100; i++)
+            {
+                var a = rand.Next(50);
+                MyDevices.Add(new Metric(48, DateTime.Now, a));
+            }
+
+            Console.WriteLine("My devices");
+            //Console.WriteLine("    Numbers of Devices:    {0}", MyDevices.Count);
+            //Console.WriteLine("    Total Devices: {0}", MyDevices.Capacity);
+            
+            PrintValues(MyDevices);
+        }
 
         public static void PrintValues(List<Metric> myList)
         {
@@ -48,35 +64,8 @@ namespace CalcEngConsole
                 {
                     Console.WriteLine("Erreur list null");
                 }
-
             }
-
         }
-
-          public static void SreenDevices()
-            {
-                //TODO passer une liste rempli de devices
-                var MyDevices = new List<Metric>();
-                MyDevices.Add(new Metric(42, DateTime.Now, 20));
-                MyDevices.Add(new Metric(42, DateTime.Now, 18));
-                MyDevices.Add(new Metric(47, DateTime.Now, 18));
-
-                var rand = new Random();
-                for (int i = 0; i<100; i++)
-                {
-                    var a = rand.Next(50);
-                    MyDevices.Add(new Metric(48, DateTime.Now, a));
-                }
-
-
-                // Displays the properties and values of the ArrayList.
-                Console.WriteLine("My devices");
-                //Console.WriteLine("    Numbers of Devices:    {0}", MyDevices.Count);
-                //Console.WriteLine("    Total Devices: {0}", MyDevices.Capacity);
-            
-                PrintValues(MyDevices);
-            }
-
 
         static MetricMoy CalcMoyenne(List<Metric> list)
         {
@@ -96,19 +85,8 @@ namespace CalcEngConsole
             }
             somme = somme/list.Count();
             return new MetricMoy(list[0].id, date, somme);
-
-
         }
-
-       
-
-
     }
-
-
-
-
-
     // ID device/ date d'insertion / valeur
     //https://o7planning.org/fr/10515/utilisation-d-une-base-de-donnees-sql-server-en-utilisant-csharp
 }
