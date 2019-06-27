170b04419e7b3b57786544106f6c9580b74c9f03
Aego <raphael.ingardin.ri@gmail.com>
2019-06-27 17:41:31 +0200

    Sender

--- Bipper/Program.cs
+++ Bipper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -7,8 +8,34 @@ namespace Bipper
 {
     class Program
     {
+        public string GenerateMessage()
+        {
+            string[] macList = { "AA:AA:AA:AA:AA:AA", "BB:BB:BB:BB:BB:BB", "CC:CC:CC:CC:CC:CC", "CC:2F:71:93:39:F1"};
+            DateTime date = DateTime.Now;
+            Random random = new Random();
+            int value = random.Next(10,50);
+            string mac = macList[random.Next(3)];
+            string message = "{\"value\": "+ value +", \"macAddress\": "+ mac +", \"date\": "+ date +"}";
+            return message;
+        }
+
+        static void LoopMaker()
+        {
+            while(true)
+            {
+                for(int i=0; i<500; i++)
+                {
+                    string message = Gene
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+
+
         static void Main()
         {
+            string message = GenerateMessage();
             var factory = new ConnectionFactory() { HostName = "10.151.129.35" };
             using (var connection = factory.CreateConnection())
             {
